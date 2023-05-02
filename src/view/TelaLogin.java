ef99b73cb1c2c56d0632820c6d977fd7f9b3e1b9
JoseMRezende <rezendinhobr18@gmail.com>
2023-05-02 20:08:41 -0300

    Versão 0.6

--- src/view/TelaLogin.java
+++ src/view/TelaLogin.java
@@ -8,12 +8,12 @@ package view;
  *
  * @author rezen
  */
-public class TelaMenu extends javax.swing.JFrame {
+public class TelaLogin extends javax.swing.JFrame {
 
     /**
-     * Creates new form TelaMenu
+     * Creates new form TelaLogin
      */
-    public TelaMenu() {
+    public TelaLogin() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class TelaMenu extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaMenu().setVisible(true);
+                new TelaLogin().setVisible(true);
             }
         });
     }
